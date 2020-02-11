8a5507d70db9258cdf54151a79e99bbc9e84ab35
Chris <mail.lamb@gmail.com>
2020-02-11 09:47:17 +0000

    init 2.1.4

--- www/js/index.js
+++ www/js/index.js
@@ -103,3 +103,35 @@ function onDeviceReady() {
 
 	admob.createBannerView();
 }
+
+var admobid = {};
+if (/(android)/i.test(navigator.userAgent)) {
+	admobid = {
+		// for Android
+		banner: 'ca-app-pub-8816517022745547/4473571984'
+		// interstitial: 'ca-app-pub-6869992474017983/1657046752'
+	};
+} else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+	admobid = {
+		// for iOS
+		banner: 'ca-app-pub-8816517022745547/4473571984'
+		// interstitial: 'ca-app-pub-6869992474017983/7563979554'
+	};
+} else {
+	admobid = {
+		// for Windows Phone
+		banner: 'ca-app-pub-8816517022745547/4473571984'
+		// interstitial: 'ca-app-pub-6869992474017983/1355127956'
+	};
+}
+
+function initApp() {
+	if (AdMob) {
+		AdMob.createBanner({
+			adId: admobid.banner,
+			position: AdMob.AD_POSITION.BOTTOM_CENTER,
+			autoShow: true
+		});
+	}
+}
+document.addEventListener('deviceready', initApp, false);
